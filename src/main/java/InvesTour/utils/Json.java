95b7621e52d74e6e02224a9e9f3ec44f8a2f4759
Idoozerii <idoozeri7@gmail.com>
2023-03-18 21:37:35 +0200

    Add Json utility class for implementation with json

---
+++ src/main/java/InvesTour/utils/Json.java
@@ -0,0 +1,26 @@
+package InvesTour.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Json {
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    @SneakyThrows
+    public JsonNode parse(String str) {
+        return mapper.readTree(str);
+    }
+
+    @SneakyThrows
+    public static JsonNode toJson(Object o) {
+        return mapper.valueToTree(o);
+    }
+
+    @SneakyThrows
+    public static <T> T fromJson(String str, Class<T> clazz) {
+        return mapper.readValue(str, clazz);
+    }
+}
