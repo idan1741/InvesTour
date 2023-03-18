d1996c659b9b99fc5adfc6c37ec93b6a7fcf12c5
Idoozerii <idoozeri7@gmail.com>
2023-03-18 21:41:30 +0200

    add new function getUserById stub

--- src/main/java/InvesTour/Services/UsersService.java
+++ src/main/java/InvesTour/Services/UsersService.java
@@ -1,9 +1,13 @@
 package InvesTour.Services;
 
+import InvesTour.Models.User;
+import InvesTour.utils.Json;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
 
-
+    public User getUserById(Long id){
+        return Json.fromJson("", User.class);
+    }
 }
