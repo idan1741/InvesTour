ce6443796fe9030570dfeee72aa350d8604b3bb1
noamGolan <noamgolan296@gmail.com>
2023-05-17 18:13:25 +0300

    Add myProfile page

--- InvesTourFront/src/components/my-profile/myProfile.component.ts
+++ InvesTourFront/src/components/my-profile/myProfile.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
-import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { addStockDialogComponent } from "src/dialogs/addStockDialog/addStockDialog.component";
-import { getStocksByUser } from "src/server-requests/stocks/stocks.actions";
+import { getStocksByUser, toggleStock } from "src/server-requests/stocks/stocks.actions";
 import { selectUserStockList } from "src/server-requests/stocks/stocks.reducer";
-import { Stock } from "../stock/stock.class";
+import { RequestConfigService } from "src/server-requests/requests.service";
+import { selectUserId, selectUsersState } from "src/server-requests/users/users.reducer";
 
 @Component({
   selector: 'myProfile',
@@ -13,43 +13,33 @@ import { Stock } from "../stock/stock.class";
   styleUrls: ['./myProfile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-  // UserStockList$ = this.store.select(selectUserStockList);
+  public UserStockList$ = this.store.select(selectUserStockList);
+  public user$ = this.store.select(selectUsersState);
+  public userId$ = this.store.select(selectUserId);
+  public websites$;
 
-
-  UserStockList$ = [{symbol: "TSLA", name: "Tesla, Inc", change: 1.17, price: 123.5, isRiseUp: true} as Stock,
-  {symbol: "SPY", name: "s&p 500", change: 0.58, price: 382.91, isRiseUp: false} as Stock,
-  {symbol: "svd", name: "Tesla, Inc", change: 1.17, price: 123.5, isRiseUp: true} as Stock,
-  {symbol: "adwafs", name: "s&p 500", change: 0.58, price: 382.91, isRiseUp: false} as Stock,
-  {symbol: "qwrfq", name: "Tesla, Inc", change: 1.17, price: 123.5, isRiseUp: true} as Stock,
-  {symbol: "gshrtfg", name: "s&p 500", change: 0.58, price: 382.91, isRiseUp: false} as Stock,
-  {symbol: "afd", name: "Tesla, Inc", change: 1.17, price: 123.5, isRiseUp: true} as Stock,
-  {symbol: "3453ty", name: "s&p 500", change: 0.58, price: 382.91, isRiseUp: false} as Stock];
-
-  constructor(private store: Store, public dialog: MatDialog, private router: Router) { }
+  constructor(private store: Store, public dialog: MatDialog, private configService: RequestConfigService) {
+  }
 
   ngOnInit(): void {
     this.store.dispatch(getStocksByUser())
+    this.user$.subscribe(user => this.websites$ = this.configService.getWebsitesByUser(user.email));
   }
 
   addStock() {
     const dialogRef = this.dialog.open(addStockDialogComponent, {
       data: {},
     });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
   }
 
-  toggle() {
-    this.router.navigateByUrl('/myWall');
-  }
-
-  remove(stock: Stock): void {
-    const index = this.UserStockList$.indexOf(stock);
-
-    if (index >= 0) {
-      this.UserStockList$.splice(index, 1);
-    }
+  remove(stock: any): void {
+    const stockSymbol = stock.symbol;
+    const stockId = stock.id;
+    this.store.dispatch(
+      toggleStock({
+        stockSymbol,
+        stockId,
+      })
+    );
   }
 }
